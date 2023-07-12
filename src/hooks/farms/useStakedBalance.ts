e148aabee4525db417dccc2bc08d0338d92545e6
Vitalsine85 <rob@creative-element.net>
2023-07-12 00:52:23 -0400

    remove old files, add walletconnect to connect-src

--- src/hooks/farms/useStakedBalance.ts
+++
@@ -1,9 +0,0 @@
-import useUserFarmInfo from './useUserFarmInfo'
-
-const useStakedBalance = (pid: number) => {
-	const userFarmInfo = useUserFarmInfo(pid)
-	// FIXME: this should handle decimals and formatting etc
-	return userFarmInfo?.amount
-}
-
-export default useStakedBalance
