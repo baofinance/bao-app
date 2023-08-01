bca78402629a72ba082942a3e6f025e9542eb0eb
Vitalsine85 <rob@creative-element.net>
2023-08-01 14:28:57 -0400

    add stakedao external farm

--- src/hooks/gauges/useStakeDaoYield.ts
+++ src/hooks/gauges/useStakeDaoYield.ts
@@ -1,33 +1,26 @@
-import { useQuery } from '@tanstack/react-query'
-import { BigNumber } from 'ethers'
+import { useEffect, useState } from 'react'
 
-// INFO: add to this to support new tokens
+export const useStakeDaoYield = () => {
+	const [apr, setApr] = useState(null)
+	const [tvl, setTvl] = useState(null)
 
-export const useStakeDaoYield = (key: string, name: string) => {
-	const { data: apr } = useQuery(
-		['@/hooks/base/useStakeDaoYield', { key }],
-		async () => {
-			const res = await fetch('https://lockers.stakedao.org/api/strategies/cache/balancer', { mode: 'no-cors' })
-			const data = await res.json()
-			const _data = data.data
-			const targetItem = _data.find((item: { key: string }) => item.key === key)
-			if (!targetItem) throw new Error(`Can't get yield for StakeDAO strategy ${name}.`)
+	useEffect(() => {
+		fetch('/api/yield/stakedao')
+			.then(response => response.json())
+			.then(data => {
+				const apr = data.apr
+				setApr(apr)
+				const tvl = data.tvl
+				setTvl(tvl)
+			})
+			.catch(error => {
+				console.error(error)
+				// Handle the error
+			})
+	}, [])
 
-			const apr = targetItem.projectedAprWithoutUserBoost
-			return apr
-		},
-		{
-			retry: true,
-			retryDelay: 1000 * 60,
-			refetchOnReconnect: true,
-			refetchInterval: 1000 * 60 * 5,
-			staleTime: 1000 * 60 * 5,
-			cacheTime: 1000 * 60 * 10,
-			keepPreviousData: true,
-			placeholderData: '',
-		},
-	)
-	return apr
+	return {
+		apr,
+		tvl,
+	}
 }
-
-export default useStakeDaoYield
--- src/pages/gauges/components/ExternalFarms.tsx
+++ src/pages/gauges/components/ExternalFarms.tsx
@@ -4,12 +4,14 @@ import Image from 'next/future/image'
 import React from 'react'
 import { isDesktop } from 'react-device-detect'
 import useAuraPool from '@/hooks/gauges/useAuraPool'
-import useStakeDaoYield from '@/hooks/gauges/useStakeDaoYield'
+import { useStakeDaoYield } from '@/hooks/gauges/useStakeDaoYield'
 
 const ExternalFarms: React.FC = () => {
 	const baoUsdLusdPool = useAuraPool('93')
 	const baoEthEthPool = useAuraPool('132')
-	const stakeDaoYield = useStakeDaoYield('b_baousd_lusd', 'baoUSD/LUSD')
+	const stakeDaoYield = useStakeDaoYield()
+
+	console.log(stakeDaoYield && stakeDaoYield)
 
 	return (
 		<>
@@ -69,6 +71,52 @@ const ExternalFarms: React.FC = () => {
 						<div className='mx-auto my-0 flex basis-1/4 flex-col items-end justify-center text-right lg:basis-1/5'>
 							<Typography variant='base' className='ml-2 inline-block font-bakbak'>
 								${getDisplayBalance(baoUsdLusdPool ? baoUsdLusdPool.tvl : 0, 0)}
+							</Typography>
+						</div>
+					</div>
+				</a>
+
+				<a
+					className='glassmorphic-card w-full px-4 py-2 duration-300 hover:cursor-pointer hover:border-baoRed hover:bg-baoRed hover:bg-opacity-20'
+					href='https://app.aura.finance/#/1/pool/93'
+				>
+					<div className='flex w-full flex-row'>
+						<div className='flex basis-1/4 lg:basis-2/5'>
+							<div className='mx-0 my-auto inline-block h-full items-center'>
+								<div className='mr-2 hidden lg:inline-block'>
+									<Image className='z-10 inline-block select-none' src='/images/tokens/baoUSD.png' alt='baoUSD' width={24} height={24} />
+									<Image className='z-20 -ml-2 inline-block select-none' src='/images/tokens/LUSD.png' alt='LUSD' width={24} height={24} />
+								</div>
+								<span className='inline-block text-left align-middle'>
+									<Typography variant='base' className='font-bakbak'>
+										baoUSD-LUSD
+									</Typography>
+								</span>
+							</div>
+						</div>
+
+						<div className='mx-auto my-0 flex basis-1/4 items-center justify-center lg:basis-1/5'>
+							<div className='mx-0 my-auto inline-block h-full items-center'>
+								<div className='mr-2 hidden lg:inline-block'>
+									<Image src={`/images/platforms/StakeDAO.png`} height={24} width={24} alt='Aura' className='mr-1 hidden lg:inline' />
+								</div>
+								<span className='inline-block text-left align-middle'>
+									<Typography variant='base' className='font-bakbak'>
+										StakeDAO
+									</Typography>
+								</span>
+							</div>
+						</div>
+
+						<div className='mx-auto my-0 flex basis-1/4 items-center justify-center lg:basis-1/5'>
+							<Typography variant='base' className={`ml-2 inline-block font-bakbak`}>
+								{getDisplayBalance(stakeDaoYield.apr ? stakeDaoYield.apr * 100 : 0, 0)}%
+							</Typography>
+						</div>
+
+						<div className='mx-auto my-0 flex basis-1/4 flex-col items-end justify-center text-right lg:basis-1/5'>
+							<Typography variant='base' className='ml-2 inline-block font-bakbak'>
+								${getDisplayBalance(stakeDaoYield.tvl ? stakeDaoYield.tvl : 0, 0)}
 							</Typography>
 						</div>
 					</div>
