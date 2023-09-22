096dca27e8b75354f03a2ce4c81deedc1d45cd30
Haruxe <haruxe@proton.me>
2023-09-21 20:58:50 -0700

    fix spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
 						BAO
 						<br /> Finance
 					</Typography>
-					<div className='mt-4 flex gap-2'>
+					<div className='my-4 flex gap-2'>
 						{/* <Icon icon='lightbulb' className='m-0 h-6 w-6 flex-none' /> */}
 						<Typography className='m-0 pr-1 font-light text-base lg:mb-4'>
 							Gain exposure to an ever-increasing number of tokens and diversify your portfolio on one of Ethereum&apos;s most decentralized
