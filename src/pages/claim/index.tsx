061a73071a718b8a69b3dcdc27e2a8ad14960c22
fabiaZ84 <fabiaz84@protonmail.com>
2025-06-06 19:57:38 +0200

    added warning polly panda end

--- src/pages/claim/index.tsx
+++ src/pages/claim/index.tsx
@@ -13,11 +13,22 @@ const ClaimPage: React.FC = () => {
 					<Typography variant='hero' className='stroke'>
 						Claim
 					</Typography>
-					<div className='flex gap-2'>
-						<Icon icon='lightbulb' className='m-0 h-6 w-6 flex-none' />
-						<Typography className='m-0 pr-1 text-base font-light tracking-tight lg:mb-4'>
-							Claim your BAOv2 tokens for locking veBao. If you are eligible, you can claim your BAOv2 tokens here.
-						</Typography>
+					<div className='flex flex-col gap-2 lg:mb-4'>
+						{/* Alert Message */}
+						<div className='flex items-start gap-2'>
+							<Icon icon='warning' className='h-6 w-6 flex-none text-red' />
+							<Typography variant='xl' className='text-red text-center font-bakbak'>
+								Panda / Polly claim has ended!
+							</Typography>
+						</div>
+
+						{/* Info Message */}
+						<div className='flex items-start gap-2'>
+							<Icon icon='lightbulb' className='m-0 h-6 w-6 flex-none' />
+							<Typography variant='xl' className='text-white text-center font-bakbak'>
+								Claim your BAOv2 tokens for locking veBao. If you are eligible, you can claim your BAOv2 tokens here.
+							</Typography>
+						</div>
 					</div>
 				</div>
 
